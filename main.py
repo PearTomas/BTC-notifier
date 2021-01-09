2806da83a14020ef0905bab97cf276a06158231f
Patrik Jakab <patrikjak@gmail.com>
2021-01-09 19:11:29 +0100

    little changes

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_rounded_per(value):
         return str(round(100 - (value / act_btc) * 100, 2)) + "%"
 
 
-print("\nNotifikácia o hodnote dôjde každých 15 minúť...\n")
+print("\nNotifikácia o hodnote dôjde každých 15 minút...\n")
 print("Aktuálna hodnota BTC je:", act_btc, "€")
 print("\nNastavenie limitov pre tento session...\n")
 min_notify = round(float(input("Ak BTC klesne pod hodnotu (€): ")), 2)
@@ -29,11 +29,10 @@ print("To je pokles o:", get_rounded_per(min_notify))
 max_notify = float(input("Ak BTC pôjde nad hodnotu (€): "))
 print("To je navýšenie o:", get_rounded_per(max_notify), "\n")
 
-actual_time = datetime.datetime.now()
-actual_time = actual_time.strftime("%H:%M:%S")
-
 
 def load_state():
+    actual_time = datetime.datetime.now()
+    actual_time = actual_time.strftime("%H:%M:%S")
     btc = 0.00
     response = requests.get(url, headers=headers)
     response = response.json()
